74c3ca8f0d87ad59e6d022420218e20fe103c5e3
Abhinandan <93651229+AE-Hertz@users.noreply.github.com>
2024-09-25 11:31:40 +0530

    init

--- auth-backend/server.js
+++ auth-backend/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import authRoutes from './routes/auth.js';
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
 
 dotenv.config();
 
